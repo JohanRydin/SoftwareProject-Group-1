2c3f6951e14f526d6ac809104d54600654a66cca
Tjasomfan <73586986+Tjasomfan@users.noreply.github.com>
2024-11-25 16:27:04 +0100

    nlkt problem solved, idk why :)

--- AIServer/initialize.py
+++ AIServer/initialize.py
@@ -2,4 +2,5 @@ import nltk
 
 #Run to download the neccessary packages of nltk
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
--- AIServer/server.py
+++ AIServer/server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-#from main import get_recommendations
+from main import get_recommendations
 
 app = FastAPI()
 
@@ -16,6 +16,6 @@ def read_recommendations(body: dict):
     if not user_data or not rows_data:
         return {"error": "Missing required data"}
 
-    #recommendations = get_recommendations(user_data, rows_data)
+    recommendations = get_recommendations(user_data, rows_data)
 
-    return {"games": ["CS2", "Baldurs Gate 3"]}
+    return {"games": ["CS2", "Baldurs Gate 3"], "user_id":user_data['id']}
