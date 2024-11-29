84bcf0ed5cfba4c8d615a5a4e643700c5c046631
JohanRydin <johanrydin00@gmail.com>
2024-11-29 14:11:06 +0100

    add: get: user/username

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -38,4 +38,9 @@ def get_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
 
-
+@app.get("/user/{username}", response_model=UserResponse)
+def get_user(username: str, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.username == username).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
