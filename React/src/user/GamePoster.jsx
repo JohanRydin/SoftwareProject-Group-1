76d40aca44f25c12022f8e89b0228f4d8c993a51
erdem garip <erdemg0919@gmail.com>
2024-12-10 22:57:39 +0100

    added image and changed modal dialog

--- React/src/user/GamePoster.jsx
+++ React/src/user/GamePoster.jsx
@@ -1,46 +1,43 @@
 import React, { useRef, useState, useEffect } from "react";
 import './GamePoster.css';
-import  {getGameImage, postGamePreference, postWishlistGame} from './Connections.jsx'
+import { getGameImage, postGamePreference, postWishlistGame } from './Connections.jsx';
 import AddIcon from '@mui/icons-material/Add';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 
+function GamePoster({ userName, gameID, image = null, name = "Cyberpunk 2077", rating = 0, description = '', genres = [], gameDict, onCardClick = null }) {
+  const [basedimage, setImage] = useState(image);
 
-function GamePoster({userName, gameID, image=null, name = "Cyberpunk 2077", rating = 0, description = '',genres=[], gameDict, onCardClick = null}) {
-    const [basedimage, setImage] = useState(image);
+  const postGamePref = () => {
+    postGamePreference(userName, gameID);
+  };
+  const postToWishlist = () => {
+    postWishlistGame(userName, gameID);
+  };
 
-    const postGamePref = () =>{
-        postGamePreference(userName, gameID)
-    }
-    const postToWishlist = () =>{
-        postWishlistGame(userName, gameID)
-    }
+  useEffect(() => {
+    const getImage = async () => {
+      const gameImage = await getGameImage(name);
+      setImage(gameImage);
+    };
+    getImage();
+  }, []);
 
-
-    useEffect(() => {
-        const getImage = async () => {
-            const gameImage = await getGameImage(name)
-            setImage(gameImage)
-        };
-        getImage()
-    }, []);
-
-   return( 
-    <div className="game-poster" >
-        {basedimage != null && <img
-            onClick={(yourmom) => {onCardClick(gameDict)}}
-            src={basedimage}
-            alt={name}
-            className="poster-image" />}
-        <div className="game-info">
-            <h3>{name}</h3>
-            <div className="buttons">
-                <button data-hover-text="Add to My List" onClick={postGamePref}><ThumbUpIcon /></button>
-                <button data-hover-text="Add to Wishlist" onClick={postToWishlist}><AddIcon /></button>
-            </div>
-                
+  return (
+    <div className="game-poster">
+      {basedimage != null && <img
+        onClick={() => { onCardClick({ ...gameDict, image: basedimage }) }}
+        src={basedimage}
+        alt={name}
+        className="poster-image" />}
+      <div className="game-info">
+        <h3>{name}</h3>
+        <div className="buttons">
+          <button data-hover-text="Add to My List" onClick={postGamePref}><ThumbUpIcon /></button>
+          <button data-hover-text="Add to Wishlist" onClick={postToWishlist}><AddIcon /></button>
         </div>
+      </div>
     </div>
-   )
+  );
 }
 
-export default GamePoster
+export default GamePoster;
--- React/src/user/Home.jsx
+++ React/src/user/Home.jsx
@@ -4,7 +4,7 @@ import GameList from './GameList'
 import { getGamePreferences, getRecommendations, getWishList } from './Connections.jsx'
 import Modal from './Modal.jsx'
 
-function Home({searchQuery, displayMyList, displayWishlist, userName}) {
+function Home({ searchQuery, displayMyList, displayWishlist, userName }) {
   const [games, setGames] = useState([]);
   const [myList, setMyList] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -16,32 +16,32 @@ function Home({searchQuery, displayMyList, displayWishlist, userName}) {
   const BASE_URL = 'https://api.rawg.io/api';
 
   useEffect(() => {
-      const fetchGames = async () => {
-        var commands = [{"similar_to_games" : "all"}, {"best_reviewed" : "Action"}, {"best_sales" : "Adventure"}] // TODO: Make logic to set commands
-        var _userName = userName
-        try {
-          if (userName == null) // TODO: If no user is logged in, set default value. Decide something better later
-          {
-            _userName = "Erik"
-            commands = [{"similar_to_games" : [9]}, {"best_reviewed" : "Action"}, {"best_sales" : "Adventure"}]
-          }
-
-          getRecommendations(_userName, commands).then(data => {
-            const games = data.response.games;
-            setGames(games);
-          })
-          
-          getGamePreferences(_userName).then(data => {
-            const _myList = data;
-            setMyList(_myList);
-          })
-
-          setLoading(false);
-        } catch (err) {
-          console.error('Error fetching games:', err);
-          setError(true);
-          setLoading(false);
+    const fetchGames = async () => {
+      var commands = [{ "similar_to_games": "all" }, { "best_reviewed": "Action" }, { "best_sales": "Adventure" }] // TODO: Make logic to set commands
+      var _userName = userName
+      try {
+        if (userName == null) // TODO: If no user is logged in, set default value. Decide something better later
+        {
+          _userName = "Erik"
+          commands = [{ "similar_to_games": [9] }, { "best_reviewed": "Action" }, { "best_sales": "Adventure" }]
         }
+
+        getRecommendations(_userName, commands).then(data => {
+          const games = data.response.games;
+          setGames(games);
+        })
+
+        getGamePreferences(_userName).then(data => {
+          const _myList = data;
+          setMyList(_myList);
+        })
+
+        setLoading(false);
+      } catch (err) {
+        console.error('Error fetching games:', err);
+        setError(true);
+        setLoading(false);
+      }
     };
 
     fetchGames();
@@ -62,19 +62,19 @@ function Home({searchQuery, displayMyList, displayWishlist, userName}) {
 
   return (
     <div className="home">
-      <Modal 
+      <Modal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
         game={selectedGame}
       />
 
       {games != [] && <div className="games-row">
-        {searchQuery != '' && (<GameList userName={userName} games={games[0]} title={searchQuery} onCardClick={handleCardClick}/>)}
-        {userName != '' && displayMyList && (<GameList userName={userName} games={myList} title ={`${userName}'s List`} onCardClick={handleCardClick}/>)}
-        {userName != '' && displayWishlist && (<GameList userName={userName} games={games[1]} title ={`${userName}'s Wishlist`} onCardClick={handleCardClick}/>)}
-        <GameList userName={userName} games={games[0]} title="Top 1 games" onCardClick={handleCardClick}/>
-        <GameList userName={userName} games={games[1]} title="Best reviewed action games" onCardClick={handleCardClick}/>
-        <GameList userName={userName} games={games[2]} title="Most popular" onCardClick={handleCardClick}/>
+        {searchQuery != '' && (<GameList userName={userName} games={games[0]} title={searchQuery} onCardClick={handleCardClick} />)}
+        {userName != '' && displayMyList && (<GameList userName={userName} games={myList} title={`${userName}'s List`} onCardClick={handleCardClick} />)}
+        {userName != '' && displayWishlist && (<GameList userName={userName} games={games[1]} title={`${userName}'s Wishlist`} onCardClick={handleCardClick} />)}
+        <GameList userName={userName} games={games[0]} title="Top 1 games" onCardClick={handleCardClick} />
+        <GameList userName={userName} games={games[1]} title="Best reviewed action games" onCardClick={handleCardClick} />
+        <GameList userName={userName} games={games[2]} title="Most popular" onCardClick={handleCardClick} />
       </div>}
     </div>
   );
