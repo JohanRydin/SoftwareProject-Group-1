d0fdc322807241589b53b52d7847d663f8500876
Tjasomfan <73586986+Tjasomfan@users.noreply.github.com>
2024-12-16 14:06:39 +0100

    Added non-functional buttons to modal

--- React/src/user/Modal.jsx
+++ React/src/user/Modal.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Modal.css';
 import starIcon from './star-icon.svg';
+import GenreButton from './GenreButton';
 
 
 export const Modal = ({ isOpen, onClose, game }) => {
   if (!isOpen) return null;
+
+  // game.genres is a string looking like "['action', 'rpg'...]". Can be parsed as list but we need to replace ' with " first
+  const genres = JSON.parse(game.genres.replace(/'/g, '"'));
 
   return (
     <div className="modal-overlay" onClick={onClose}>
@@ -18,7 +22,18 @@ export const Modal = ({ isOpen, onClose, game }) => {
         <img src={game.image} alt={game.gamename} className="modal-image" />
         <hr className="modal-divider" />
         <p className="modal-description">{game["description"]}</p>
-        <p className="modal-genres">Genres: {game["genres"]}</p>
+        <div className="modal-genres">
+          <div className="genres-list">
+            {genres.map((genre) => (
+              <GenreButton
+                key={genre}
+                genre={genre}
+                isToggled={false}
+                onToggle={() => {}}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
