f49426c39604e973dfcd26e830ef4d1ce9a33649
Tjasomfan <73586986+Tjasomfan@users.noreply.github.com>
2025-01-08 13:25:50 +0100

    sum mary

--- React/src/user/RowSelector.jsx
+++ React/src/user/RowSelector.jsx
@@ -6,6 +6,12 @@ function randString(list)
     return list[randomIndex];
 }
 
+function RandomInsert(commands, titles, object, title) {
+    const randomIndex = Math.floor(Math.random() * (commands.length + 1));
+    commands.splice(randomIndex, 0, object);
+    titles.splice(randomIndex, 0, title);
+}
+
 /*  Get some commands for rows based on user preferences
     @note - Selects games randomly based on this information
     @param gamePrefs - The games that the user likes as list of gameobjects
@@ -13,28 +19,31 @@ function randString(list)
     @param wishList - The wishlist of the user
     @returns [commands, titles] where commands is a list of dicts and titles a list of strings
 */
-export function SelectRows(gamePrefs, genrePrefs, wishList)
+export function SelectRows(gamePrefs, genrePrefs, wishList, genres)
 {
-    var commands = []
-    var titles = []
+    var commands = [{"best_sales" : "Action"}, {"best_sales" : "Adventure"}]
+    var titles = ["Top Sellers", "Hot Right Now"]
 
     if (gamePrefs.length > 0)
     {
-        commands = [...commands, {"similar_to_games" : "all"}]
-        titles = [...titles, "Similar To What You Play"]
+        //commands = [...commands, {"similar_to_games" : "all"}]
+        //titles = [...titles, "Similar To What You Play"]
+        RandomInsert(commands, titles, {"similar_to_games" : "all"}, "Similar To What You Play")
     }
     if (gamePrefs.length > 0)
     {
         const index = Math.floor(Math.random() * gamePrefs.length);
         const gameID = gamePrefs[index]['id']
-        commands = [...commands, {"similar_to_games" : [gameID]}]
-        titles = [...titles, "Because You Liked '".concat(gamePrefs[index]['gamename']).concat("'")]
+       // commands = [...commands, {"similar_to_games" : [gameID]}]
+        //titles = [...titles, "Because You Liked '".concat(gamePrefs[index]['gamename']).concat("'")]
+        RandomInsert(commands, titles, {"similar_to_games" : [gameID]}, "Because You Liked '".concat(gamePrefs[index]['gamename']).concat("'"))
     }
     if (genrePrefs.length > 0)
     {
         const genre = randString(genrePrefs)
-        commands = [...commands, {"similar_to_genre" : genre}]
-        titles = [...titles, "Games From The '".concat(genre).concat("' Genre")]
+        //commands = [...commands, {"similar_to_genre" : genre}]
+        //titles = [...titles, "Games From The '".concat(genre).concat("' Genre")]
+        RandomInsert(commands, titles, {"similar_to_genre" : genre}, "Games From The '".concat(genre).concat("' Genre"))
     }
     if (genrePrefs.length > 0)
     {
@@ -42,8 +51,9 @@ export function SelectRows(gamePrefs, genrePrefs, wishList)
         if (filtered_genres.length > 0)    // Only some genres contain ranking info in AIServer
         {
             const genre = randString(filtered_genres)
-            commands = [...commands, {"best_reviewed" : genre}]
-            titles = [...titles, "Best Reviewed '".concat(genre).concat("' Games")]
+            //commands = [...commands, {"best_reviewed" : genre}]
+            //titles = [...titles, "Best Reviewed '".concat(genre).concat("' Games")]
+            RandomInsert(commands, titles, {"best_reviewed" : genre}, "Best Reviewed '".concat(genre).concat("' Games"))
         }
     }
     if (genrePrefs.length > 0)
@@ -52,8 +62,21 @@ export function SelectRows(gamePrefs, genrePrefs, wishList)
         if (filtered_genres.length > 0)    // Only some genres contain ranking info in AIServer
         {
             const genre = randString(filtered_genres)
-            commands = [...commands, {"best_sales" : genre}]
-            titles = [...titles, "Most Popular '".concat(genre).concat("' Games")]
+            //commands = [...commands, {"best_sales" : genre}]
+            //titles = [...titles, "Most Popular '".concat(genre).concat("' Games")]
+            RandomInsert(commands, titles, {"best_sales" : genre}, "Most Popular '".concat(genre).concat("' Games"))
+        }
+    }
+
+    if (true) {
+        var filtered_genres = genres.filter(genre => !(genrePrefs.includes(genre)))
+        filtered_genres = filtered_genres.filter(genre => ranking_genres.includes(genre))
+        if (filtered_genres.length > 0)    // Only some genres contain ranking info in AIServer
+        {
+            const genre = randString(filtered_genres)
+            //commands = [...commands, {"best_sales" : genre}]
+            //titles = [...titles, "Maybe You Like These"]
+            RandomInsert(commands, titles, {"best_sales" : genre}, "Maybe You Like These")
         }
     }
 
